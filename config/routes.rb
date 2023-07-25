94b457c831ca06f66d3c2fc6fa0757deffb86dcc
minori chikano <bcloudabc@gmail.com>
2023-07-25 13:35:41 +0900

    add project モデルの生成とビュー・コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  get 'projects/new'
+  get 'projects/edit'
+  get 'projects/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
