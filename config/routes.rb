6a45ad839d4848a6a2de4ab5d653ca5fddc87c95
minori chikano <bcloudabc@gmail.com>
2023-07-25 13:43:22 +0900

    change rootをproject#indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-  get 'projects/new'
-  get 'projects/edit'
-  get 'projects/show'
+  root 'projects#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
