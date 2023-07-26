4ba3b6b850bd945b93c32dee0756f304a599f056
minori chikano <bcloudabc@gmail.com>
2023-07-26 15:45:47 +0900

    rails adminの導入

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'devise-i18n'
 gem 'rexml'
+gem 'rails_admin', '~> 3.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'projects#index'
   resources :projects
   devise_for :users
