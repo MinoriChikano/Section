fb16b8eba59d6dd0ad54f0bd705581dd3fca2178
minori chikano <bcloudabc@gmail.com>
2023-07-29 16:05:47 +0900

    add audios新規画面を仮作成し、接続

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -1,8 +1,19 @@
 class AudiosController < ApplicationController
   def index
+    @audios = Audio.all
   end
 
   def new
+    @audios = Audio.new
+  end
+
+  def create
+    @audios = Audio.new
+    if @audio.save
+      redirect_to audios_path, notice: "ミュージックを作成しました"
+    else
+      render :new
+    end
   end
 
   def show
