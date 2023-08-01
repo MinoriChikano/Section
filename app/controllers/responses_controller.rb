885347b4677f53cf18b746ef02655b9780d2942f
minori chikano <bcloudabc@gmail.com>
2023-08-01 17:40:29 +0900

    add レスポンス機能で、コメントできるようにした

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,4 +1,19 @@
 class ResponsesController < ApplicationController
   def create
+    @audio = Audio.find(params[:audio_id])
+    @response = @audio.responses.build(response_params)
+    respond_to do |format|
+      if @response.save
+        format.html { redirect_to audio_path(@audio) }
+      else
+        format.html { redirect_to audio_path(@audio), notice: '投稿できませんでした'}
+      end
+    end
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'responses/create'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'projects#index'
   resources :projects
@@ -7,6 +6,7 @@ Rails.application.routes.draw do
     member do
       get:download
     end
+    resources :responses
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
