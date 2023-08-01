1044f22fc08eb3686a3c66767f159ee3f7ca47e5
minori chikano <bcloudabc@gmail.com>
2023-08-01 18:27:11 +0900

    add Ajaxでコメントできるようにすることができた

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,13 +1,44 @@
 class ResponsesController < ApplicationController
+  before_action :set_audio, only: [:create, :edit, :update]
   def create
     @audio = Audio.find(params[:audio_id])
     @response = @audio.responses.build(response_params)
     respond_to do |format|
       if @response.save
-        format.html { redirect_to audio_path(@audio) }
+        format.js { render :index }
       else
         format.html { redirect_to audio_path(@audio), notice: '投稿できませんでした'}
       end
+    end
+  end
+
+  def edit
+    @response = @audio.responses.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @response = @audio.responses.find(params[:id])
+      respond_to do |format|
+        if @response.update(response_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit }
+        end
+      end
+  end
+
+  def destroy
+    @response = Response.find(params[:id])
+    @response.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'コメントが削除されました'
+      format.js { render :index }
     end
   end
 
@@ -15,5 +46,9 @@ class ResponsesController < ApplicationController
 
   def response_params
     params.require(:response).permit(:comment)
+  end
+
+  def set_audio
+    @audio = Audio.find(params[:audio_id])
   end
 end
