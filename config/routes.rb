44f4bb42d6f5296065da718d77604610d68ed28a
minori chikano <bcloudabc@gmail.com>
2023-08-04 00:36:37 +0900

    add joinsコントローラの生成

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       end
     end
   end
+  resources :joins, only: [:create]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/joins_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class JoinsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
