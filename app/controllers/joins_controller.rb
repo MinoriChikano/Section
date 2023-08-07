db18997bf0aecca65826bc02b6021b6ce6b9fbd5
minori chikano <bcloudabc@gmail.com>
2023-08-08 01:06:21 +0900

    change プロジェクト作成者はそのプロジェクトに重複して追加できないようにする

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -2,7 +2,7 @@ class JoinsController < ApplicationController
   before_action :authenticate_user!
   before_action :email_exist?, only: [:create]
   before_action :user_exist?, only: [:create]
-  # before_action :owner_exist?, only: [:create]
+  before_action :owner?, only: [:create]
 
   def new
     @project = Project.find(params[:project_id])
@@ -30,7 +30,7 @@ class JoinsController < ApplicationController
 
   def user_exist?
     project = find_project(params[:project_id].keys.first)
-    redirect_to project_url(project), notice:"このユーザーは存在していません" unless User.exists?(email: params[:email])
+    redirect_to project_url(project), notice:"このユーザーは存在しないか、入力欄が空になっています" unless User.exists?(email: params[:email])
   end
 
   def email_exist?
@@ -38,10 +38,10 @@ class JoinsController < ApplicationController
     redirect_to project_url(project), notice: "このユーザーはすでに登録されています" if project.members.exists?(email: params[:email])
   end
 
-  # def owner_exist?
-  #   project = find_project(params[:project_id].keys.first)
-  #   redirect_to project_url(project), notice: "このユーザーはオーナ-として登録されています" if project.user
-  # end
+  def owner?
+    project = find_project(params[:project_id].keys.first)
+    redirect_to project_url(project), notice: "このユーザーはオーナ-として登録されています" if project.user.email == params[:email]
+  end
 
   # def email_reliable?(address)
   #   address.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
