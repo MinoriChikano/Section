b9ca48af3399f94fa173153c98bd191f6d069d41
minori chikano <bcloudabc@gmail.com>
2023-08-10 14:47:53 +0900

    add 念の為、ゲストユーザーを削除できないようにする

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
     end
   end
   resources :joins, only: [:new, :create]
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
