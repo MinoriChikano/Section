7f28fe651d4d7d722d8de8bc61c951b18ec68f1a
minori chikano <bcloudabc@gmail.com>
2023-08-10 14:18:32 +0900

    add deviceのコントローラ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,9 @@ class User < ApplicationRecord
   def self.find_by_email(email)
     user = find_by(email: email)
   end
+
+  find_or_create_by!(email: 'guest@example.com') do |user|
+    user.password = SecureRandom.urlsafe_base64
+    user.name = SecureRandom.urlsafe_base64
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,8 @@ Rails.application.routes.draw do
   end
   resources :joins, only: [:new, :create]
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
