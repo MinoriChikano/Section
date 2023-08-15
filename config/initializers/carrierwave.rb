24aa9a43abc63e9b2eaa44fc75e58a7be98b4613
minori chikano <bcloudabc@gmail.com>
2023-08-15 19:16:21 +0900

    change S3関連で設定の変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,9 +19,9 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'sectionapp'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sectionapp'
     when 'development'
-      config.fog_directory = 'sectionapp'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.fog_directory = 'sectiondev'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sectiondev'
   end
 end
