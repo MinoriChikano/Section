6c3e7cdf507380baa895aa86e842e4281a81d85f
minori chikano <bcloudabc@gmail.com>
2023-08-15 23:48:20 +0900

    change rootを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'tops/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'projects#index'
+  root 'tops#index'
   resources :projects
   resources :audios do
     member do
