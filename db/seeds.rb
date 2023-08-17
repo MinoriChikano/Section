d8c920527dee83febbe8f8a0d6a04af33b0df140
minori chikano <bcloudabc@gmail.com>
2023-08-18 08:58:10 +0900

    add ローカル環境でのシードデータ投入完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 User.create!(
   name: "Chikano",
   email: "chikano@email.com",
