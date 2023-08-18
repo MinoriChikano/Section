06bb4cc33401ceea1830177774f470d73c552047
minori chikano <bcloudabc@gmail.com>
2023-08-19 02:58:15 +0900

    projectのnameカラムを追加

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,7 @@ class Project < ApplicationRecord
   has_many :members, through: :joins, source: :user
   has_many :audios, dependent: :destroy
   validates :name, presence: true
+  validates :name, length: { maximum: 30 }
 
   # def join_member(user)
   #   Joins.create(user: user)
