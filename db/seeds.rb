e133584770ba348ca5d714faf2f3e476795f4960
minori chikano <bcloudabc@gmail.com>
2023-08-18 17:12:38 +0900

    シードデータ訂正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(
-  name: "Chikano",
-  email: "chikano@email.com",
-  password: "123456",
-  admin: true
-)
+# User.create!(
+#   name: "Chikano", 
+#   email: "chikano@email.com",
+#   password: "123456",
+#   admin: true
+# )
 
-users = [
-  { name: "Hasshy", email: "hasshy@email.com", password: "123456", admin: false },
-  { name: "Urushi", email: "urushi@email.com", password: "123456", admin: false },
-  { name: "Muramatsu", email: "muramatsu@email.com", password: "123456", admin: false },
-  { name: "Ozawa", email: "ozawa@email.com", password: "123456", admin: false },
-  { name: "Okimon", email: "okimon@email.com", password: "123456", admin: false },
-  { name: "Yoshida", email: "yoshida@email.com", password: "123456", admin: false },
-  { name: "Ishii", email: "ishii@email.com", password: "123456", admin: false },
-  { name: "Yamada", email: "yamada@email.com", password: "123456", admin: false }
-]
+# users = [
+#   { name: "Hasshy", email: "hasshy@email.com", password: "123456", admin: false },
+#   { name: "Urushi", email: "urushi@email.com", password: "123456", admin: false },
+#   { name: "Muramatsu", email: "muramatsu@email.com", password: "123456", admin: false },
+#   { name: "Ozawa", email: "ozawa@email.com", password: "123456", admin: false },
+#   { name: "Okimon", email: "okimon@email.com", password: "123456", admin: false },
+#   { name: "Yoshida", email: "yoshida@email.com", password: "123456", admin: false },
+#   { name: "Ishii", email: "ishii@email.com", password: "123456", admin: false },
+#   { name: "Yamada", email: "yamada@email.com", password: "123456", admin: false }
+# ]
 
-users.each do |user|
-  User.create!(user)
-end
+# users.each do |user|
+#   User.create!(user)
+# end
 
 projects = [
-  { user_id: 1, name: "without" },
-  { user_id: 1, name: "KANARI" },
-  { user_id: 2, name: "Cityspeak" },
-  { user_id: 1, name: "チカノ個人用" },
-  { user_id: 2, name: "はっしー個人用" }
+  { user_id: 3, name: "without" },
+  { user_id: 3, name: "KANARI" },
+  { user_id: 4, name: "Cityspeak" },
+  { user_id: 3, name: "チカノ個人用" },
+  { user_id: 4, name: "はっしー個人用" }
 ]
 
 projects.each do |project|
@@ -41,15 +41,15 @@ projects.each do |project|
 end
 
 joins = [
-  { project_id: 1, user_id: 3 },
-  { project_id: 1, user_id: 4 },
-  { project_id: 1, user_id: 5 },
-  { project_id: 2, user_id: 6 },
-  { project_id: 2, user_id: 7 },
-  { project_id: 2, user_id: 8 },
-  { project_id: 3, user_id: 1 },
-  { project_id: 3, user_id: 7 },
-  { project_id: 3, user_id: 9 }
+  { project_id: 4, user_id: 5 },
+  { project_id: 4, user_id: 6 },
+  { project_id: 4, user_id: 7 },
+  { project_id: 5, user_id: 8 },
+  { project_id: 5, user_id: 9 },
+  { project_id: 5, user_id: 11 },
+  { project_id: 6, user_id: 3 },
+  { project_id: 6, user_id: 9 },
+  { project_id: 6, user_id: 11 }
 ]
 
 joins.each do |join|
@@ -57,11 +57,11 @@ joins.each do |join|
 end
 
 audios = [
-  { project_id: 1, user_id: 1, title: "2023song1", bpm: 182, key: "D", comment: "新曲です。途中で拍子が3拍子に変わります", file: File.open('./app/assets/files/2023song_1.mp3') },
-  { project_id: 4, user_id: 1, title: "2023song3", bpm: 100, key: "E", comment: "とりあえずギターのみ", file: File.open('./app/assets/files/2023song3.mp3') },
-  { project_id: 1, user_id: 1, title: "2023song5", bpm: 159, key: "D", comment: "新曲です。Urushiは左チャンネルのギターをコピーしておいて!", file: File.open('./app/assets/files/2023song5-2.mp3') },
-  { project_id: 2, user_id: 1, title: "2023プリプロ", bpm: 82, key: "D", comment: "今日のリズム隊のプリプロです", file: File.open('./app/assets/files/newsong.mp3') },
-  { project_id: 4, user_id: 1, title: "2023song6", bpm: 120, key: "E", comment: "新曲(ドラム打ち込みのみ)", file: File.open('./app/assets/files/2023song6drum.mp3') }
+  { project_id: 4, user_id: 3, title: "2023song1", bpm: 182, key: "D", comment: "新曲です。途中で拍子が3拍子に変わります", file: File.open('./app/assets/files/2023song_1.mp3') },
+  { project_id: 7, user_id: 3, title: "2023song3", bpm: 100, key: "E", comment: "とりあえずギターのみ", file: File.open('./app/assets/files/2023song3.mp3') },
+  { project_id: 4, user_id: 3, title: "2023song5", bpm: 159, key: "D", comment: "新曲です。Urushiは左チャンネルのギターをコピーしておいて!", file: File.open('./app/assets/files/2023song5-2.mp3') },
+  { project_id: 5, user_id: 3, title: "2023プリプロ", bpm: 82, key: "D", comment: "今日のリズム隊のプリプロです", file: File.open('./app/assets/files/newsong.mp3') },
+  { project_id: 7, user_id: 3, title: "2023song6", bpm: 120, key: "E", comment: "新曲(ドラム打ち込みのみ)", file: File.open('./app/assets/files/2023song6drum.mp3') }
 ]
 
 audios.each do |audio|
@@ -69,11 +69,11 @@ audios.each do |audio|
 end
 
 responses = [
-  { audio_id: 3, user_id: 3, comment: "オッケー!耳コピしとく" },
-  { audio_id: 3, user_id: 4, comment: "ドラムだけの音声もらえる?" },
-  { audio_id: 3, user_id: 1, comment: "送る！", file: File.open('./app/assets/files/2023song5.mp3') },
-  { audio_id: 4, user_id: 6, comment: "もう少しテンポ遅い方がいいね" },
-  { audio_id: 5, user_id: 1, comment: "一旦お蔵入り" }
+  { audio_id: 6, user_id: 5, comment: "オッケー!耳コピしとく" },
+  { audio_id: 6, user_id: 6, comment: "ドラムだけの音声もらえる?" },
+  { audio_id: 6, user_id: 3, comment: "送る！", file: File.open('./app/assets/files/2023song5.mp3') },
+  { audio_id: 7, user_id: 8, comment: "もう少しテンポ遅い方がいいね" },
+  { audio_id: 8, user_id: 3, comment: "一旦お蔵入り" }
 ]
 
 responses.each do |response|
