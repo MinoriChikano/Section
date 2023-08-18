208b531f099b9aa2bdb00bd54100c9b056999f6d
minori chikano <bcloudabc@gmail.com>
2023-08-19 02:06:49 +0900

    change nameカラムにバリデーションをかける

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
   has_many :responses
   has_many :joins, dependent: :destroy
   has_many :join_projects, through: :joins, source: :project
+  validates :name, presence: true
   validates :email, presence: true
-
+  
   def self.find_by_email(email)
     user = find_by(email: email)
   end
