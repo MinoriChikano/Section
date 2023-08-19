172096aa71280b5b97c4dd251c1aa975c748e6d0
minori chikano <bcloudabc@gmail.com>
2023-08-19 12:03:12 +0900

    test モデルテストの失敗ケースを実施。問題なし

--- spec/models/audio_spec.rb
+++ spec/models/audio_spec.rb
@@ -22,13 +22,6 @@ RSpec.describe "オーディオモデル機能", type: :model do
         expect(audio).to be_valid
       end
     end
-  
-    context 'BPMが文字列の場合' do
-      it 'バリデーションにひっかかる' do
-        audio = FactoryBot.build(:audio, bpm: "テスト")
-        expect(audio).to be_invalid
-      end
-    end
 
     context 'BPMが30を下回る数値の場合' do
       it 'バリデーションにひっかかる' do
@@ -54,6 +47,57 @@ RSpec.describe "オーディオモデル機能", type: :model do
     context 'BPMが適切な数値の場合' do
       it 'バリデーションが通る' do
         audio = FactoryBot.build(:audio, bpm: 30)
+        expect(audio).to be_valid
+      end
+    end
+  end
+
+  describe '失敗テスト' do
+    context 'タイトルが空の場合' do
+      it 'バリデーションにひっかかる' do
+        audio = FactoryBot.build(:audio, title: "失敗")
+        expect(audio).to be_invalid
+      end
+    end
+
+    context 'タイトルが30文字以上の場合' do
+      it 'バリデーションにひっかかる' do
+        audio = FactoryBot.build(:audio, title: "失敗ぽんぽこぴーのぽんぽこなのちょうきゅうめいのちょうすけ")
+        expect(audio).to be_invalid
+      end
+    end
+
+    context '適切なタイトルの場合' do
+      it 'バリデーションを通過する' do
+        audio = FactoryBot.build(:audio, title: "")
+        expect(audio).to be_valid
+      end
+    end
+
+    context 'BPMが30を下回る数値の場合' do
+      it 'バリデーションにひっかかる' do
+        audio = FactoryBot.build(:audio, bpm: "31")
+        expect(audio).to be_invalid
+      end
+    end
+
+    context 'BPMが255を上回る数値の場合' do
+      it 'バリデーションにひっかかる' do
+        audio = FactoryBot.build(:audio, bpm: 255)
+        expect(audio).to be_invalid
+      end
+    end
+
+    context 'BPMが空白の場合' do
+      it 'バリデーションが通る' do
+        audio = FactoryBot.build(:audio, bpm: "失敗")
+        expect(audio).to be_valid
+      end
+    end
+
+    context 'BPMが適切な数値の場合' do
+      it 'バリデーションが通る' do
+        audio = FactoryBot.build(:audio, bpm: 29)
         expect(audio).to be_valid
       end
     end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -23,4 +23,27 @@ RSpec.describe "プロジェクトモデル機能", type: :model do
       end
     end
   end
+
+  describe '失敗テスト' do
+    context 'プロジェクト名が空の場合' do
+      it 'バリデーションにひっかかる' do
+        project = FactoryBot.build(:project, name: "テスト")
+        expect(project).to be_invalid
+      end
+    end
+
+    context 'プロジェクト名が30文字以上の場合' do
+      it 'バリデーションにひっかかる' do
+        project = FactoryBot.build(:project, name: "じゅげむじゅげむごこうの失敗")
+        expect(project).to be_invalid
+      end
+    end
+
+    context '適切なプロジェクト名の場合' do
+      it 'バリデーションを通過する' do
+        project = FactoryBot.build(:project, name: "")
+        expect(project).to be_valid
+      end
+    end
+  end
 end
