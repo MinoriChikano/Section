1ca060f695ca14e1cd7192f37ac0259d271c47bc
minori chikano <bcloudabc@gmail.com>
2023-08-20 22:32:59 +0900

    change 失敗確認用テストのコメントアウト

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -24,26 +24,26 @@ RSpec.describe "プロジェクトモデル機能", type: :model do
     end
   end
 
-  describe '失敗テスト' do
-    context 'プロジェクト名が空の場合' do
-      it 'バリデーションにひっかかる' do
-        project = FactoryBot.build(:project, name: "テスト")
-        expect(project).to be_invalid
-      end
-    end
+  # describe '失敗テスト' do
+  #   context 'プロジェクト名が空の場合' do
+  #     it 'バリデーションにひっかかる' do
+  #       project = FactoryBot.build(:project, name: "テスト")
+  #       expect(project).to be_invalid
+  #     end
+  #   end
 
-    context 'プロジェクト名が30文字以上の場合' do
-      it 'バリデーションにひっかかる' do
-        project = FactoryBot.build(:project, name: "じゅげむじゅげむごこうの失敗")
-        expect(project).to be_invalid
-      end
-    end
+  #   context 'プロジェクト名が30文字以上の場合' do
+  #     it 'バリデーションにひっかかる' do
+  #       project = FactoryBot.build(:project, name: "じゅげむじゅげむごこうの失敗")
+  #       expect(project).to be_invalid
+  #     end
+  #   end
 
-    context '適切なプロジェクト名の場合' do
-      it 'バリデーションを通過する' do
-        project = FactoryBot.build(:project, name: "")
-        expect(project).to be_valid
-      end
-    end
-  end
+  #   context '適切なプロジェクト名の場合' do
+  #     it 'バリデーションを通過する' do
+  #       project = FactoryBot.build(:project, name: "")
+  #       expect(project).to be_valid
+  #     end
+  #   end
+  # end
 end
